5c51d16b9f03c4fb76e9093838c0e274d854f802
avinash-25 <avinashranjan268@gmail.com>
2023-09-30 18:46:29 +0530

    initial commit

--- Prdct.js
+++ Prdct.js
@@ -1,13 +1,24 @@
 import React from 'react';
-// import './App.css';
 import Wishlist from './Wishlist';
 
 function Prdct() {
   // Sample product data
   const products = [
-    { name: 'Product 1', description: 'Description for Product 1' },
-    { name: 'Product 2', description: 'Description for Product 2' },
-    { name: 'Product 3', description: 'Description for Product 3' },
+    {
+      img: './SlidesPic/pic2.jpg',
+      name: 'Product 1',
+      description: 'Description for Product 1'
+    },
+    {
+      img: './SlidesPic/pic1.jpg',
+      name: 'Product 2',
+      description: 'Description for Product 2'
+    },
+    {
+      img: './SlidesPic/pic3.jpg',
+      name: 'Product 3',
+      description: 'Description for Product 3'
+    },
   ];
 
   return (
--- Wishlist.js
+++ Wishlist.js
@@ -3,16 +3,15 @@ import './Wishlist.css';
 
 const Wishlist = ({ products }) => {
   const [wishlist, setWishlist] = useState([]);
-  const [addedToWishlist, setAddedToWishlist] = useState(false); // New state variable
+  const [addedToWishlist, setAddedToWishlist] = useState(false);
 
   const addToWishlist = (product) => {
     if (!wishlist.includes(product)) {
       setWishlist([...wishlist, product]);
-      setAddedToWishlist(true); // Set the state to true when added to wishlist
-      // Simulate page reload by resetting it after a delay
+      setAddedToWishlist(true);
       setTimeout(() => {
         setAddedToWishlist(false);
-      }, 2000); // Reset after 2 seconds (adjust as needed)
+      }, 2000);
     }
   };
 
@@ -22,32 +21,36 @@ const Wishlist = ({ products }) => {
   };
 
   return (
-    <div className="wishlist-container">
-      <h1>My Wishlist</h1>
-      {addedToWishlist && (
-        <div className="added-message">
-          <p>Added to wishlist!</p>
-        </div>
-      )}
-      <div className="products-container">
-        {products.map((product, index) => (
-          <div className="product" key={index}>
-            <h3>{product.name}</h3>
-            <p>{product.description}</p>
-            <button onClick={() => addToWishlist(product)}>Your WishList is here!</button>
-            <button onClick={() => removeFromWishlist(product)}>Remove from Wishlist</button>
+    <>
+      <div className="wishlist-container">
+        <h1>My Wishlist</h1>
+        {addedToWishlist && (
+          <div className="added-message">
+            <p>Added to wishlist!</p>
           </div>
-        ))}
-      </div>
-      <div className="wishlist-items">
-        <h2>Your wishlist is here:</h2>
-        <ul>
-          {wishlist.map((item, index) => (
-            <li key={index}>{item.name}</li>
+        )}
+        <div className="products-container">
+          {products.map((product, index) => (
+            <div className="product" key={index}>
+              <h3>{product.name}</h3>
+              <p>{product.description}</p>
+              <button onClick={() => addToWishlist(product)}>Add to Wishlist</button>
+            </div>
           ))}
-        </ul>
+        </div>
+        <div className="wishlist-items">
+          <h2>Your wishlist is here:</h2>
+          <ul>
+            {wishlist.map((item, index) => (
+              <li key={index}>
+                {item.name}
+                <button onClick={() => removeFromWishlist(item)}>Remove from Wishlist</button>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
