d8e0d4da593accbd909080491c95af2f9a65df63
avinash-25 <avinashranjan268@gmail.com>
2023-10-01 16:33:54 +0530

    initial commit

--- Prdct.js
+++ Prdct.js
@@ -5,17 +5,17 @@ function Prdct() {
   // Sample product data
   const products = [
     {
-      img: './SlidesPic/pic2.jpg',
+      img: '/SlidesPic/pic1.jpg',
       name: 'Product 1',
       description: 'Description for Product 1'
     },
     {
-      img: './SlidesPic/pic1.jpg',
+      img: '/SlidesPic/pic2.jpg',
       name: 'Product 2',
       description: 'Description for Product 2'
     },
     {
-      img: './SlidesPic/pic3.jpg',
+      img: '/SlidesPic/pic3.jpg',
       name: 'Product 3',
       description: 'Description for Product 3'
     },
